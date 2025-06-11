a8a628397dcb1694c99fdf8b2bad465a50de07bb
ellenliujia <Ellen.liu@richemont.com>
2025-06-11 18:40:33 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  lintOnSave: false
+  lintOnSave: false,
+  configureWebpack: {
+    devtool: 'source-map' // 启用 source map，便于调试 .vue 文件
+  }
 })
